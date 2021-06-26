6748874cea882cf2640d96aa9bbe5230e8f4d658
Francisco <fnaveira11@gmail.com>
2021-06-26 16:54:52 -0400

    commit tree f15a4f35c8aa7c3e9e0971f140ed268ba5794e3b

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,31 @@ class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    session[:session_hello] ||= 3
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
+  # def index
+  #   
+
+  #   if :pageviews_remaining >=1
+  #     articles = Article.all.includes(:user).order(created_at: :desc)
+  #     render json: articles, each_serializer: ArticleListSerializer
+  #   end
+    
+  # end
+
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:pageviews_remaining]||= 3
+    session[:pageviews_remaining] -= 1
+
+    if session[:pageviews_remaining] == 0
+      render json: { error: "Maximum pageview limit reached"},status: :unauthorized
+    else
+      article = Article.find(params[:id])
+      render json: article
+    end
+    
   end
 
   private
